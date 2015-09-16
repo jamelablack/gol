91a2f59c54791a2cd8e6367ca314a622adaf0612
Jamela Black <jamela.black@gmail.com>
2015-09-15 23:46:25 -0400

    Cell populates neighbors

--- lib/gol.rb
+++ lib/gol.rb
@@ -1,6 +1,15 @@
 class Cell
+  attr_accessor :x, :y
 
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
   def neighbors
     []
+  end
+
+  def populate_at(x,y)
+    Cell.new(x,y)
   end
 end
--- spec/gol_spec.rb
+++ spec/gol_spec.rb
@@ -1,9 +1,19 @@
 require 'gol'
 
 describe 'Game of Life' do
-  let(:cell) { Cell.new }
+  let(:cell) { Cell.new(0,0) }
+
+  context "cell lifecycle" do
+    it "populates cell at (x,y)" do
+      live_cell = cell.populate_at(3,5)
+
+      expect(live_cell).to be_instance_of (Cell)
+      expect(live_cell.x).to eq (3)
+      expect(live_cell.y).to eq (5)
+    end
+  end
 
   it "Rule #1: Any cell with fewer than 2 live neighbors, dies" do
-    expect(cell.neighbors).to eq []
+    expect(cell.neighbors.count).to eq (0)
   end
 end
